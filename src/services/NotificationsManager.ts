3b8a2d42af0b380d3157e339c0a9485fbed6b8c7
Hana Mohan <unamashana@gmail.com>
2023-08-07 22:23:58 +0200

    fixes keys for storing magicbell preferences for vscode

--- src/services/NotificationsManager.ts
+++ src/services/NotificationsManager.ts
@@ -1,33 +1,38 @@
-import MagicBell from 'magicbell';
-import * as vscode from "vscode"
+import MagicBell from "magicbell";
+import * as vscode from "vscode";
 
-import {Store} from "../state";
+import { Store } from "../state";
 
 const config = vscode.workspace.getConfiguration("CodeInbox");
 
 export default class NotificationsManager {
-    private store: Store;
-    private magicBell: MagicBell;
-    constructor(store: Store) {
-        const githubUsername = config.get("githubUsername") as string;
-        const magicBellApiKey = config.get("magicBellApiKey") as string;
-        const magicBellApiUrl = config.get("magicBellApiUrl") || "https://api.magicbell.com";
-        if (!githubUsername || !magicBellApiKey) {
-            throw new Error("Missing configuration for CodeInbox extension. Please set your GitHub username and MagicBell API key in the settings.")
-        }
-        this.store = store;
-        this.magicBell = new MagicBell({
-            apiKey: magicBellApiKey,
-            userExternalId: "moxley01",
-            host: magicBellApiUrl as string
-        })
+  private store: Store;
+  private magicBell: MagicBell;
+  constructor(store: Store) {
+    const githubUsername = config.get("githubUsername") as string;
+    const magicBellApiKey = config.get("magicBellApiKey") as string;
+    const magicBellApiUrl =
+      config.get("magicBellApiUrl") || "https://api.magicbell.com";
+    if (!githubUsername || !magicBellApiKey) {
+      throw new Error(
+        "Missing configuration for CodeInbox extension. Please set your GitHub username and MagicBell API key in the settings."
+      );
     }
-    public async init() {
-        const {notifications} = await this.magicBell.notifications.list()
-        this.store.getState().setNotifications(notifications as any)
-        this.magicBell.listen().forEach(async event => {
-            const newNotification = await this.magicBell.notifications.get(event.data.id)
-            this.store.getState().addNotification(newNotification)
-        })
-    }
+    this.store = store;
+    this.magicBell = new MagicBell({
+      apiKey: magicBellApiKey,
+      userExternalId: githubUsername,
+      host: magicBellApiUrl as string,
+    });
+  }
+  public async init() {
+    const { notifications } = await this.magicBell.notifications.list();
+    this.store.getState().setNotifications(notifications as any);
+    this.magicBell.listen().forEach(async (event) => {
+      const newNotification = await this.magicBell.notifications.get(
+        event.data.id
+      );
+      this.store.getState().addNotification(newNotification);
+    });
+  }
 }
