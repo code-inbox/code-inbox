562413346869a156e58de1dfd697eec4b5b72d9d
Matt Oxley <matt_ox@hotmail.com>
2023-08-10 00:16:31 +0200

    cleanup

--- dynamicContributes.ts
+++ dynamicContributes.ts
@@ -17,22 +17,22 @@ const getRootPath = () => {
 function getFrameworkViews() {
     const framework = process.env.FRAMEWORK || "react"
     const mapToExtension = {
-      react: "?sx",
-      svelte: "svelte",
+        react: "?sx",
+        svelte: "svelte",
     }
     if (!mapToExtension[framework]) {
-      throw new Error(`Unsupported framework: ${framework}`)
+        throw new Error(`Unsupported framework: ${framework}`)
     }
     const rootPath = getRootPath()
 
     const folderPath = path.resolve(rootPath, "src/views")
     // give me all the framework files in the folder
     return fs
-      .readdirSync(folderPath)
-      .filter((file) =>
-        new RegExp(`.${mapToExtension[framework]}$`).test(file)
-      )
-      .map((file) => ({ name: file.split(".")[0], path: path.resolve(folderPath, file) }))
+        .readdirSync(folderPath)
+        .filter((file: string) =>
+            new RegExp(`.${mapToExtension[framework]}$`).test(file)
+        )
+        .map((file: string) => ({name: file.split(".")[0], path: path.resolve(folderPath, file)}))
 
 }
 
@@ -42,13 +42,13 @@ const viewsPaths = getFrameworkViews()
 const json = require(path.resolve("package.json"))
 json.contributes.views.container = []
 
-viewsPaths.forEach(({ name: viewPath }) => {
-  const viewName = viewPath.split(".")[0]
-  json.contributes.views.container.push({
-    id: viewName,
-    name: viewName,
-    type: "webview",
-    initialSize: 4,
-  })
-  saveFile(path.resolve("package.json"), JSON.stringify(json, null, 2))
+viewsPaths.forEach(({name: viewPath}: {name: string}) => {
+    const viewName = viewPath.split(".")[0]
+    json.contributes.views.container.push({
+        id: viewName,
+        name: viewName,
+        type: "webview",
+        initialSize: 4,
+    })
+    saveFile(path.resolve("package.json"), JSON.stringify(json, null, 2))
 })
--- globals.d.ts
+++ globals.d.ts
@@ -1,13 +1,6 @@
-declare module 'getFrameworkViews' {
-    export function getFrameworkViews(): Promise<{
-        name: string;
-        path: string;
-    }>
-}
-
 // declare process.env with the framework var
 declare namespace NodeJS {
     interface ProcessEnv {
-        FRAMEWORK: string;
+        FRAMEWORK: "react" | "svelte";
     }
 }
